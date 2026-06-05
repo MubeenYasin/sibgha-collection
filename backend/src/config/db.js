6cd6bba25b332f85902a4002b99b24926bc410d0
mubeenyasin <mubeen.yasin@gmail.com>
2026-06-06 02:06:51 +0300

    Authentication System Fully Working!

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,10 @@
 // ✅ Correct
 import mongoose from 'mongoose';
+import config from './config.js';
 
 const connectDB = async () => {  // no next here
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(config.MONGODB_URI);
         console.log(`MongoDB Connectedd : ${conn.connection.host}`);
     } catch (error) {
         console.log(`MongoDB Error: ${error.message}`);
